7dd8c5c676830337c58c4832e5460f199b144d84
ericdoi <ricekido@gmail.com>
2015-02-28 08:26:58 -0800

    Removed unused pages

--- pages.py
+++ pages.py
@@ -27,55 +27,6 @@ def mindex():
     """
     return flask.render_template("map.html")
 
-
-@app.route("/data")
-@app.route("/data/<int:ndata>")
-def data(ndata=100):
-    """
-    On request, this returns a list of ``ndata`` randomly made data points.
-
-    :param ndata: (optional)
-        The number of data points to return.
-
-    :returns data:
-        A JSON string of ``ndata`` data points.
-
-    """
-    x = 10 * np.random.rand(ndata) - 5
-    y = 0.5 * x + 0.5 * np.random.randn(ndata)
-    A = 10. ** np.random.rand(ndata)
-    c = np.random.rand(ndata)
-    return json.dumps([{"_id": i, "x": x[i], "y": y[i], "area": A[i],
-        "color": c[i]}
-        for i in range(ndata)])
-
-@app.route("/gdata")
-@app.route("/gdata/<float:mux>/<float:muy>")
-def gdata(ndata=100,mux=.5,muy=0.5):
-    """
-    On request, this returns a list of ``ndata`` randomly made data points.
-    about the mean mux,muy
-
-    :param ndata: (optional)
-        The number of data points to return.
-
-    :returns data:
-        A JSON string of ``ndata`` data points.
-
-    """
-
-    """Instead of getting values from URL, try from request object"""
-    mux = request.args.get('mux', '')
-    muy = request.args.get('muy', '')
-
-    x = np.random.normal(mux,.5,ndata)
-    y = np.random.normal(muy,.5,ndata)
-    A = 10. ** np.random.rand(ndata)
-    c = np.random.rand(ndata)
-    return json.dumps([{"_id": i, "x": x[i], "y": y[i], "area": A[i],
-        "color": c[i]}
-        for i in range(ndata)])
-
 @app.route("/getdists")
 def getdists():
     """
